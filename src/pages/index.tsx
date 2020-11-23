ef3b4736957c82d18e461822bc7d4fc56f763ce5
Alicia Foureaux <aliciafoureauxlucas@gmail.com>
2020-11-23 09:50:00 -0300

    Client side fetching example added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from 'react';
+import { ThemeConsumer } from 'styled-components';
 import { Title } from '../styles/pages/Home';
 
+interface IProduct {
+  id: string;
+  title: string;
+}
+
 export default function Home() {
+  const [recommended, setRecommended] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended').then(response => {
+      response.json().then(data => {
+        setRecommended(data);
+      });
+    });
+  }, []);
+
   return (
-    <Title>Hello World!</Title>
+    <div>
+      <section>
+        <Title>Products</Title>
+      </section>
+
+      <ul>
+        {recommended.map((recommendedProduct) => {
+          return (
+            <li key={recommendedProduct.id}>
+              {recommendedProduct.title}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
